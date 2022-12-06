8b023a9ef89a958111ba8537f267cc5bb9e38530
root <root@vultr.guest>
2022-12-06 19:16:31 +0000

    fix shell

--- bin/renew_cert.sh
+++ bin/renew_cert.sh
@@ -4,12 +4,15 @@
 # bin/renew_cert.sh "trojan-docker的安装目录"
 #------------------------------------------------
 
-
 WORKDIR=$1
 if [ -z "${WORKDIR}" ] ; then
   WORKDIR="/usr/local/trojan-docker"
 fi
 
+docker-compose down
+
 /usr/bin/certbot renew
 cat /etc/letsencrypt/live/*/fullchain.pem > ${WORKDIR}/cert/fullchain.pem
 cat /etc/letsencrypt/live/*/privkey.pem > ${WORKDIR}/cert/privkey.pem
+
+docker-compose up -d
